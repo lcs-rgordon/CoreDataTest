956365fbeab44cc20b9c21d0a56224c7643adf6e
Russell Gordon <rgordon@lcs.on.ca>
2020-12-27 08:10:28 -0500

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by Russell Gordon on 2020-12-27.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
